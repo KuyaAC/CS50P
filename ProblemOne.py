2bca97f55ddfff21f5d9871c8194087e3e3cf66c
Allen Carl <aciesnet02@gmail.com>
2024-09-05 11:02:50 +0800

    Lecture 2 task 2/5

--- ProblemOne.py
+++ ProblemOne.py
@@ -5,3 +5,17 @@ if x == "42" or x == "forty two" or x == "forty-two":
     print("Yes")
 else:
     print("No")
+
+#If the greeting starts with “hello”, output $0. If the greeting starts with an “h” (but not “hello”), output $20. Otherwise, output $100. Ignore any leading whitespace in the user’s greeting, and treat the user’s greeting case-insensitively.
+greet = input("Greeting: ").lower()
+words = greet.split()
+first_word = words[0]
+
+if first_word.startswith("hello"):
+    print("$0")
+
+elif greet[0] == "h":
+    print("$20")
+
+else:
+    print("$100")
