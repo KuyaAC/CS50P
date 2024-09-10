29537af958bf2469155c261b9531b4fda59a3c3e
Allen Carl <aciesnet02@gmail.com>
2024-09-10 10:32:05 +0800

    HackerankChallenge 2

--- HackerRank.py
+++ HackerRank.py
@@ -5,7 +5,7 @@
 # If  is even and in the inclusive range of  to , print Not Weird
 # If  is even and in the inclusive range of  to , print Weird
 # If  is even and greater than , print Not Weird
-
+#ANSWER:
 if __name__ == '__main__':
     n = int(input().strip())
     if n % 2 == 0 and n >= 6 and n <= 20:
@@ -14,3 +14,18 @@ if __name__ == '__main__':
         print("Weird")
     else:
         print("Not Weird")
+
+# Task
+# The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+#ANSWER:
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(f"{a + b}")
+    print(f"{a - b}")
+    print(f"{a * b}")
